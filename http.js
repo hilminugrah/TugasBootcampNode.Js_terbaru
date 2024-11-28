6e317fe38b030ab40046339794dd8fa3c4c1e602
hilminugrah <hilminugrahs@gmail.com>
2024-11-28 13:43:25 +0700

    mnbhkan file

---
+++ http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+
+
+http
+  .createServer((req, res) => {
+    const url = req.url;
+    console.log(url)
+    
+    const renderHtml = (path, res) => {
+    fs.readFile(path, (err,data) => {    
+        if (url === '/about') {
+            renderHtml(".views/about.html", res)
+            } else if (url === '/contact') {
+            renderHtml(".views/contact.html", res)
+            } else {
+              res.writeHead(404, { 'content-type': 'text/html' });
+              res.write('hello world');
+              res.end();
+            }
+    })
+    }
+
+    
+  })
+  .listen(3000, () => {
+    console.log('server is running on port 3000');
+  });
