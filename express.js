42cf1299afe65d2bb0139b763a69098e1ef67736
hilminugrah <hilminugrahs@gmail.com>
2024-11-29 11:44:56 +0700

    menambahkan tugas json hari jumat

--- express.js
+++ express.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const port = 3000;
 const path = require('path')
+const fs = require('fs')
+const filefath = ('data/contact.json');
 
 app.set("view engine", "ejs");
 
@@ -20,21 +22,27 @@ app.get("/about",(req,res) => {
 app.get("/contact",(req,res) => {
     // res.sendFile('views/contact.html', { root: __dirname });
     // res.render('contact')
-    cont = [{
-       nama: "hilmi",
-       mobile: "0812232433",
-    },
-    {
-      nama: "hilmi",
-      mobile: "0812232433",
-   },
-   {
-      nama: "hilmi",
-       mobile: "081223243[3",
+  //   cont = [{
+  //      nama: "hilmi",
+  //      mobile: "0812232433",
+  //   },
+  //   {
+  //     nama: "hilmi",
+  //     mobile: "0812232433",
+  //  },
+  //  {
+  //     nama: "hilmi",
+  //      mobile: "081223243[3",
+  //  }
+  //   ]
+  let cont = []
+   if(fs.existsSync(filefath)){
+    cont = JSON.parse(fs.readFileSync(filefath,'utf-8'))
    }
-    ]
-    res.render("contact", cont);
+    res.render("contact", {item: cont});
 })
+
+  
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
