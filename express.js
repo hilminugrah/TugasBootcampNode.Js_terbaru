abf700a0da8e6d97f1eb06a9862f5ed8fdff74f9
hilminugrah <hilminugrahs@gmail.com>
2024-11-29 11:09:28 +0700

    menambahkan tugas contact hari jumat

--- express.js
+++ express.js
@@ -3,16 +3,37 @@ const app = express()
 const port = 3000;
 const path = require('path')
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.sendFile('views/index.html', { root: __dirname });
+    // res.sendFile('views/index.html', { root: __dirname });
+    // res.render('index')
+    const nama = "hilmi nugraha"
+    res.render("index", {nama})
   })
 
 app.get("/about",(req,res) => {
-    res.sendFile('views/about.html', { root: __dirname });
+    // res.sendFile('views/about.html', { root: __dirname });
+    res.render('about')
 })
 
 app.get("/contact",(req,res) => {
-    res.sendFile('views/contact.html', { root: __dirname });
+    // res.sendFile('views/contact.html', { root: __dirname });
+    // res.render('contact')
+    cont = [{
+       nama: "hilmi",
+       mobile: "0812232433",
+    },
+    {
+      nama: "hilmi",
+      mobile: "0812232433",
+   },
+   {
+      nama: "hilmi",
+       mobile: "081223243[3",
+   }
+    ]
+    res.render("contact", cont);
 })
 
 app.listen(port, () => {
